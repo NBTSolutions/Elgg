3d517396c9988de0b1bc6158dac843574ef5ca4c
tjwebb <travis@traviswebb.com>
2013-07-23 00:43:05 -0400

    added cache folder

--- run.sh
+++ run.sh
@@ -4,3 +4,4 @@ bash boot.sh
 echo "Include /app/www/config/httpd/*.conf" >> /app/apache/conf/httpd.conf
 chmod 777 /app/www/engine/heroku_settings.php
 chmod 777 /app/www/elgg_data
+chmod 777 /app/www/cache
