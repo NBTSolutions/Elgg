55bb613a9399185a2a82af3bd22ee4523723291f
noelmiciano <noel.miciano@nbtsolutions.com>
2013-07-26 15:58:46 -0400

    added weather widget and tweaked header.css & header.php

--- mod/weatherblur_theme/views/default/page/elements/header.php
+++ mod/weatherblur_theme/views/default/page/elements/header.php
@@ -16,13 +16,59 @@ echo elgg_view('core/account/login_dropdown');
 	<div class="elgg-page-inner">
 		<div class="header-content">
 		
-			<ul id="top-nav">
-				<li id="weather-widget"></li>
-				<li><a href="#">Home</a></li>
-				<li><a href="#">About Us</a></li>
+			<div id="top-nav">
+				<!--WEATHER WIDGET-->
+				<div id="wxWrap">
+					<span id="wxIntro"></span>
+					<span id="wxIcon2"></span>
+					<span id="wxTemp"></span>
+				</div>
+				<a href="#">Home</a>
+				<a href="http://islandinstitute.org/weatherblur.php" target="_blank">About Us</a>
 				<!--<li id="avatar"></li>-->					
 				<!--<li><a href="#">My Account</a><i class="icon-chevron-down"></i></li>-->	
-			</ul><!--End top nav-->
+				
+				<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script>
+<script type="text/javascript">
+// javascript will go here
+$(function(){
+
+	var url = "http://www.geoplugin.net/json.gp?jsoncallback=?";
+
+	// get the location by ip
+	$.getJSON(url, function(data){
+		if(data['geoplugin_status'] == '200'){
+			// Do something with the data
+			var ip = (data['geoplugin_request']);
+			var lat = (data['geoplugin_latitude']);
+			var lon = (data['geoplugin_longitude']);
+			var place = (data['geoplugin_city']);
+			
+			url2 = 'https://api.metwit.com/v2/weather/?location_lat='+lat+'&location_lng='+lon ;
+			
+			
+			//get the forecast
+			$.getJSON(url2, function(forecast)
+			{	
+					
+					var tempf = Math.floor(((forecast.objects[0].weather.measured.temperature - 273) * 9/5) + 32);
+					var img = forecast.objects[0].icon;
+					$('#wxIcon').css({
+						backgroundPosition: '-' + (61 * place) + 'px 0'
+					}).attr({
+						title: place
+					});
+					$('#wxIntro').append(place);
+					$('#wxIcon2').append('<img src="'+img+'" width="34" height="34" title="' + place + '" />');
+					$('#wxTemp').html(tempf + '&deg;F' );
+			});	
+			
+		}
+	});
+});
+</script>
+
+			</div><!--End top nav-->
 			
 			<?php
 				// Header Logo
