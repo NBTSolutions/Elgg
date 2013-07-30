d0609e6ecc4cc70ba053bdeae78a9a4df4c16b00
Jesse V Griffis <jesse.griffis@nbtsolutions.com>
2013-07-30 11:13:34 -0400

    update title on the featured member section based on user's profile type

--- mod/weatherblur_theme/views/default/homepage/featured_member.php
+++ mod/weatherblur_theme/views/default/homepage/featured_member.php
@@ -5,12 +5,17 @@ if (count($featured_users) == 0) {
 	return;
 }
 $fuser = $featured_users[0];
+$title = 'Member';
+if ($fuser->custom_profile_type) {
+	$title = get_entity($fuser->custom_profile_type)->getTitle();
+}
+
 ?>
 <!-----------------------------------------------
 				FEATURED MEMBER
 ------------------------------------------------->
 <div id="featured-member">
-	<h2>Featured Member</h2>
+	<h2>Featured <?php print $title; ?></h2>
 	<a class="icon" href="<?php print $fuser->getURL(); ?>"><img src="<?php print $fuser->getIconURL('large'); ?>" /></a>
 	<a class="name" href="<?php print $fuser->getURL(); ?>"><?php print $fuser->get('name'); ?></a>
 	<?php print $fuser->get('description'); ?>
