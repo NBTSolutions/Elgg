d1cdfa6d63ca5bd68d123beb5737ed407ba24029
jlongnbt <john.longanecker@nbtsolutions.com>
2013-08-02 15:03:23 -0400

    reverted this file, we need to revist what type of files we want to show here

--- mod/file/pages/file/world.php
+++ mod/file/pages/file/world.php
@@ -13,32 +13,11 @@ $limit = get_input("limit", 10);
 
 $title = elgg_echo('file:all');
 
-// this is probably not the most elgg-efficient way to get these IDs:
-$name_id = '';
-$value_id = '';
-$name_meta = elgg_get_metadata(array(
-	'metadata_name' => 'file category'
-));
-if (count($name_meta) > 0) {
-	$name_id = $name_meta[0]->__get('name_id');
-}
-$value_meta = elgg_get_metadata(array(
-	'metadata_value' => 'proposal'
-));
-if (count($value_meta) > 0) {
-	$value_id = $value_meta[0]->__get('value_id');
-}
-
-$dbprefix = elgg_get_config('dbprefix');
-
-$content = elgg_list_entities_from_metadata(array(
+$content = elgg_list_entities(array(
 	'type' => 'object',
 	'subtype' => 'file',
 	'limit' => $limit,
-	'full_view' => FALSE,
-	'wheres' => array(
-		"NOT EXISTS ( SELECT 1 FROM {$dbprefix}metadata md WHERE md.entity_guid = e.guid AND md.name_id = $name_id AND md.value_id = $value_id)"
-	)
+	'full_view' => FALSE
 ));
 if (!$content) {
 	$content = elgg_echo('file:none');
