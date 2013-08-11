ee38ae98f87b6a51eebb05013d67359dc879cb2c
jlongnbt <john.longanecker@nbtsolutions.com>
2013-08-11 13:45:34 -0400

    force a persistent login. This will allow the is_logged_in rest call to work consistently

--- views/default/forms/login.php
+++ views/default/forms/login.php
@@ -24,8 +24,8 @@
 
 <div class="elgg-foot">
 	<label class="mtm float-alt">
-		<input type="checkbox" name="persistent" value="true" />
-		<?php echo elgg_echo('user:persistent'); ?>
+		<input type="hidden" name="persistent" value="true" />
+		<?php // echo elgg_echo('user:persistent'); ?>
 	</label>
 	
 	<?php echo elgg_view('input/submit', array('value' => elgg_echo('login'))); ?>
