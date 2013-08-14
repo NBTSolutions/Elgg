7921ba4d3562e80f9d79d9822bdae27b168b5d76
jlongnbt <john.longanecker@nbtsolutions.com>
2013-08-14 13:39:49 -0400

    cleaned up pdf filename

--- mod/investigations/views/default/investigations/sidebar/proposal.php
+++ mod/investigations/views/default/investigations/sidebar/proposal.php
@@ -7,13 +7,20 @@ $prop = elgg_get_entities_from_relationship(array(
 	'inverse_relationship' => true
 ));
 
+$regex = '/[^a-zA-Z0-9_-]/';
+
+// assuming all props are pdfs
+$filename = $inv->name . '-' . $prop[0]->getGUID(); 
+$filename = preg_replace($regex, '-', $filename) . '.pdf';
+
+$val = preg_replace($regex, '', $val);
 if (count($prop) > 0) {
 ?>
 <div class="elgg-module elgg-module-aside proposal-block">
 	<div class="proposal">
 		<?php echo elgg_view('output/url', array(
 			'text' => 'View Original Proposal',
-			'value' => 'file/download/' . $prop[0]->getGUID() . '/' . $inv->name . '-' . $prop[0]->getGUID() .'.pdf', // assuming all props are pdfs
+			'value' => 'file/download/' . $prop[0]->getGUID() . '/' . $filename,
 			'is_trusted' => true
 		)); ?>
 	</div>
