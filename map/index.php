36bcad840015355b698097d96c302b17c31dd926
jlongnbt <john.longanecker@nbtsolutions.com>
2013-08-28 08:56:07 -0400

    add env vars to fullscreen map and dct redirect pages

--- map/index.php
+++ map/index.php
@@ -1,2 +1,5 @@
 <?php
- header('Location: http://nbt-static.s3-website-us-east-1.amazonaws.com/weatherblur/map/unstable/');
+$app_env = getenv("APP_ENV");
+$app_env = $app_env ? $app_env : "unstable";
+
+header('Location: http://nbt-static.s3-website-us-east-1.amazonaws.com/weatherblur/map/'.$app_env.'/');
