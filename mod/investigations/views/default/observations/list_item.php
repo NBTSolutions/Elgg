7dab9242ee7131a06bd2e2a6647f94ca2935d1bf
jlongnbt <john.longanecker@nbtsolutions.com>
2013-09-02 11:41:32 -0400

    fixed timestamp

--- mod/investigations/views/default/observations/list_item.php
+++ mod/investigations/views/default/observations/list_item.php
@@ -17,7 +17,7 @@ if (count($e) > 0) {
         <a href="<?php echo elgg_get_site_url(); ?>observation/<?php echo $obs->get("agg_id"); ?>">
             <img class="obs_image" src='<?php echo elgg_get_site_url(); ?>_graphics/wb-small-data-icons.png'>
             <p class="obs_owner">By <?php print $obs->getOwnerEntity()->get('name'); ?></p>
-            <p class="obs_date">On <?php echo date('F nS, Y g:i:s A', $obs->get('time_created') + (3600 * (1 - date('I', $comment->time_created)))); ?></p>
+            <p class="obs_date">On <?php echo date('F jS, Y g:i:s A', $obs->get('time_created') + (3600 * (1 - date('I', $comment->time_created)))); ?></p>
         </a>
     </div>
 </li>
