06f4d03dc9655077e6a6b2ef97f9960c6431d557
jlongnbt <john.longanecker@nbtsolutions.com>
2013-09-10 08:27:15 -0400

    changed enterdata iframe url

--- mod/weatherblur_theme/pages/enterdata.php
+++ mod/weatherblur_theme/pages/enterdata.php
@@ -17,7 +17,7 @@
 	
 	$content = '<div class="wb-body">
 	<h2 style="text-align:center;padding: 20px">Enter Data</h2>
-    <iframe src="http://s3.amazonaws.com/nbt-static/weatherblur/collect/'.$app_env.'/index.html"></iframe>';
+    <iframe src="'.elgg_get_site_url().'dct/index.html"></iframe>';
 	
 	$canvas_area = elgg_view_layout('default', array('content' => $content));
 	echo elgg_view_page($title, $canvas_area);
