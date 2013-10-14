32b8576cc780c6801eb3962e3219745a5c560a51
jlongnbt <john.longanecker@nbtsolutions.com>
2013-10-14 09:27:17 -0400

    ugly fix to bug that is for some reason declaring this function/plugin twice

--- mod/weatherblur_user_obs/views/default/widgets/wbuserobs/content.php
+++ mod/weatherblur_user_obs/views/default/widgets/wbuserobs/content.php
@@ -3,37 +3,38 @@
  * WeatherBlur User Obs Widget
  */
  
- function elgg_view_observation_widget_list($entities, $vars = array(), $offset = 0, $limit = 10, $full_view = true, $list_type_toggle = true, $pagination = true) {
+if(!function_exists('elgg_view_observation_widget_list')) {
+     function elgg_view_observation_widget_list($entities, $vars = array(), $offset = 0, $limit = 10, $full_view = true, $list_type_toggle = true, $pagination = true) {
 
-	if (!is_int($offset)) {
-		$offset = (int)get_input('offset', 0);
-	}
+        if (!is_int($offset)) {
+            $offset = (int)get_input('offset', 0);
+        }
 
-	// list type can be passed as request parameter
-	$list_type = get_input('list_type', 'list');
-	if (get_input('listtype')) {
-		elgg_deprecated_notice("'listtype' has been deprecated by 'list_type' for lists", 1.8);
-		$list_type = get_input('listtype');
-	}
+        // list type can be passed as request parameter
+        $list_type = get_input('list_type', 'list');
+        if (get_input('listtype')) {
+            elgg_deprecated_notice("'listtype' has been deprecated by 'list_type' for lists", 1.8);
+            $list_type = get_input('listtype');
+        }
 
-	$defaults = array(
-		'items' => $entities,
-		'full_view' => false,
-		'pagination' => true,
-		'list_type' => $list_type,
-		'list_type_toggle' => false,
-		'offset' => $offset,
-	);
+        $defaults = array(
+            'items' => $entities,
+            'full_view' => false,
+            'pagination' => true,
+            'list_type' => $list_type,
+            'list_type_toggle' => false,
+            'offset' => $offset,
+        );
 
-	$vars = array_merge($defaults, $vars);
+        $vars = array_merge($defaults, $vars);
 
-	if ($vars['list_type'] != 'list') {
-		return elgg_view('page/components/gallery', $vars);
-	} else {
-		return elgg_view('page/components/observation_list_widget', $vars);
-	}
-}
- 
+        if ($vars['list_type'] != 'list') {
+            return elgg_view('page/components/gallery', $vars);
+        } else {
+            return elgg_view('page/components/observation_list_widget', $vars);
+        }
+    }
+} 
  
 $num = $vars['entity']->num_display;
 
