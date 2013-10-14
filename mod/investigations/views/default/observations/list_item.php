909dc248611532e14a3435168eb785163f24b0a4
jlongnbt <john.longanecker@nbtsolutions.com>
2013-10-14 09:38:15 -0400

    for some reason investigation is blank and causes errors on the profile page.

--- mod/investigations/views/default/observations/list_item.php
+++ mod/investigations/views/default/observations/list_item.php
@@ -22,7 +22,9 @@ $inv = get_entity($inv_guid);
         <a href="<?php echo elgg_get_site_url(); ?>observation/<?php echo $obs->get("agg_id"); ?>">
             <img class="obs_image" src='<?php echo elgg_get_site_url(); ?>_graphics/wb-small-data-icons.png'>
             <p class="obs_owner">I recorded an Observation</p>
+            <?php if($inv) { ?>
             <p class="obs_owner">For <?php print $inv->get('name'); ?></p>
+            <?php } ?>
             <p class="obs_date">On <?php echo date('F nS, Y g:i:s A', $obs->get('time_created') + (3600 * (1 - date('I', $comment->time_created)))); ?></p>
         </a>
     </div>
