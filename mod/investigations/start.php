dea11c54b2b0e696351f448323c14d3368e2f88b
NathanMarq <NathanMarq@gmail.com>
2014-04-02 18:41:35 -0400

    added more advisor fields for get members call and get invs call

--- mod/investigations/start.php
+++ mod/investigations/start.php
@@ -2878,7 +2878,8 @@ function get_inv_by_id($id) {
         "advisor" => array(
             "username" => $e ? $e[0]->get("username") : "",
             "displayname" => $e ? $e[0]->get("name") : "",
-            "image" => $e ? $e[0]->getIcon("medium") : ""
+            "image" => $e ? $e[0]->getIcon("medium") : "",
+            "id" => $e ? $e[0]->guid : ""
         ),
         "image" => $result->getIcon("large"),
         "description" => $result->description,
@@ -3628,6 +3629,7 @@ function get_members($page, $search) {
             "displayname" => $result->name,
             "username" => $result->username,
             "icon" => $result->getIconUrl("large"),
+            "medium_icon" => $result->getIconUrl("medium"),
             "profile_type" => $profile_type ? $profile_type->getTitle() : '',
             "school" => $result->school,
             "id" => $result->guid
